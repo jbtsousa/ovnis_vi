9769c0579e3b32b48c10a213cbb1b6d94024d3f7
Joana Sousa <jbtsousa@gmail.com>
2022-05-24 11:55:33 +0100

    comentarios

--- calculos_copy3.js
+++ calculos_copy3.js
@@ -5,7 +5,9 @@ d3.csv("/data/ufo_sights.csv").then(function (data) {
     /** * Array auxiliar apenas com as formas **/
     var shapes_rep = []
     for (var i = 0; i < data.length; i++) {
-        shapes_rep.push(data[i].shape);
+        if (data[i].shape != '') {
+            shapes_rep.push(data[i].shape);
+        }
     }
     let arr_shapes = [];
     shapes_rep.forEach((c) => {
@@ -26,11 +28,14 @@ d3.csv("/data/ufo_sights.csv").then(function (data) {
     /*** FREQUÊNCIA DE AVISTAMENTOS POR ESTADO POR FORMA ***/
     let freq_formas = [];
 
-    /*** TEMPOS POR ESTADO POR FORMA ***/
+    /*** TEMPOS E COMENTÁRIOS POR ESTADO POR FORMA ***/
     let tempos = [];
 
     /**** ARRAY Q TEM INFO QUE VAI PARA D3 *****/
     let new_ufo_data = [];
+    let tempos_aux = [];
+    let tempos_max = [];
+    let interv_tempo_aux = [];
 
 
     /**** DENTRO DO UFO DATA *****/
@@ -39,6 +44,8 @@ d3.csv("/data/ufo_sights.csv").then(function (data) {
 
         freq_formas[state] = [];
         tempos[state] = [];
+        interv_tempo_aux[state] = [];
+
         Object.entries(freq_formas).forEach(([state2, value2]) => {
 
             if (state == state2) {
@@ -55,50 +62,174 @@ d3.csv("/data/ufo_sights.csv").then(function (data) {
             }
         });
 
-        //TEMPOS
+        //TEMPOS E COMENTÁRIOS
+        var interv1, interv2, interv3, interv4;
         for (var i = 0; i < arr_shapes.length; i++) {
+
+             Object.entries(new_ufo_data).forEach(([estado, avist]) => {
+                for (var i = 0; i < avist["frequencia"]; i++) {
+
+                    if (avist["tempos"][i] >= 1 && avist["tempos"][i] < 30) {
+                        interv1+=1;
+                    }
+                    else if (avist["tempos"][i] >= 30 && avist["tempos"][i] < 180) {
+                         interv2+=1;
+                    }
+                    else if (avist["tempos"][i] >= 180 && avist["tempos"][i] < 600) {
+                         interv3+=1;
+                    }
+                    else if (avist["tempos"][i] >= 600) {
+                        interv4+=1;
+                    }
+                }
+            }); 
+
+
+            /***calculo auxiliar intervalos tempos por estado por forma** */
+            interv_tempo_aux[state].push({
+                forma: arr_shapes[i],
+                '1a30': interv1,
+                '30a180': interv2,
+                '180a600': interv3,
+                '+600': interv4
+            });
+
             tempos[state].push({
                 forma: arr_shapes[i],
-                tempos: []
+                tempos: [],
+                comentarios: []
             });
         }
 
         for (var i = 0; i < arr_shapes.length; i++) {
             for (j = 0; j < avist.length; j++) {
-                if (avist[j].shape == tempos[state][i]["forma"]){
+                if (avist[j].shape == tempos[state][i]["forma"]) {
                     tempos[state][i]["tempos"].push(avist[j].duration);
+                    tempos[state][i]["comentarios"].push(avist[j].comments);
                 }
-
             }
         }
+        tempos_aux[state] = [];
+        tempos_max[state] = [];
 
         /****  INFO QUE VAI PARA D3 *****/
 
         new_ufo_data[state] = [];
-
         for (var i = 0; i < arr_shapes.length; i++) {
             new_ufo_data[state].push({
                 forma: arr_shapes[i],
                 frequencia: freq_formas[state][arr_shapes[i]],
-                tempos: tempos[state][i]["tempos"]
+                tempos: tempos[state][i]["tempos"],
+                comment: tempos[state][i]["comentarios"]
             });
+            tempos_aux[state].push(freq_formas[state][arr_shapes[i]]);
         }
+
+        /***calculo auxiliar tempo maximo por estado** */
+        let max;
+        Object.entries(tempos_aux).forEach(([estado, tempos]) => {
+            max = d3.max(tempos);
+            Object.entries(tempos_max).forEach(([estado2, max2]) => {
+                tempos_max[state] = max;
+            })
+        });
+
+
+
+
+
+
+        /******* DESENHO DO GRÁFICO - Evgheni  *******/
+        var width = 600;
+        var height = 600;
+        var radius = 300;
+        var thickness = 15;
+
+        //aqui estamos só a fazer com um estado
+        var pieData = new_ufo_data["ab"];
+
+        var pie = d3.pie()
+            .value(function (d) {
+                return d.abs;
+            })
+
+        var arc = d3.arc()
+            .innerRadius(30)
+            .outerRadius(radius)
+            .padAngle(2)
+            .padRadius(2);
+
+        //let cor= xxx
+        //função que define forma como cor é dada?
+
+
+        let band = d3.scaleBand();
+
+        var chart = d3.select('#chart').append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(' + (width - radius) + ',' + (height - radius) + ')')
+            .selectAll('path')
+            .data(pie(pieData, (d) => {
+                return {
+                    pie: pie(d),
+                    dados: d
+                }
+            }))
+            .enter()
+            .append('g') //para cada setor um g, para cada abs
+            .attr('class', 'slice');
+
+        var slices = chart.selectAll('g.slice') //substituição de d3 por chart porque assim vai buscar dados ao data joint e não antes de fazer join, iu seja, html, sem dados agarrados
+            .data((sector) => {
+                //console.log("sector:",sector);
+                //exemplo do prof do q devia dar [{q: 1000, dur: 10}, { q: 1000, dur: 60}, 1000, 1000, 10]
+                //var avistamentos = avistamentos(sector.dados);
+                //FALTA FUNÇÃO DE AVISTAMENTOS
+                //esta que pus a seguir foi introduzida na aula:
+                var avistamentos = sector.data.tempos;
+                //console.log("avist:", avistamentos);
+                band.domain(avistamentos)
+                    .range([10, 300])
+                //.range(rangeAvistamentos(avistamentos.length)) // 10 => {20 }
+                return {
+                    inner: band(sector.avistamentos),
+                    outer: band(sector.data) + band.bandwidth(),
+                    //cor: cor(avistamentos), //depois impor regra para cor
+                    angInf: sector.startAngle,
+                    angSup: sector.endAngle,
+                    dados: sector.dados
+                }
+            })
+            .enter()
+            .append('g')
+            .attr('class', 'arc')
+            .attr('fill', '#0FF285');
+
+
+        let arco = slices.selectAll('g.arc') //aqui vamos buscar o joint anterior que criou mais dados importantes para aqui
+            .data(function (dados) {
+                console.log("dados:", dados);
+                return dados;
+            })
+            .enter()
+            .append('path')
+            .attr('d', (barra) => {
+
+                //console.log("barra:", barra);
+                arc.innerRadius(barra.inner)
+                    .outerRadius(barra.outer)
+                    .startAngle(barra.startAngle)
+                    .endAngle(barra.endAngle)
+            })
+            .attr('fill', '#F5F5F5');
 
     });
 
-    console.log('freq_formas por estado');
-    console.log(freq_formas);
-    
-    console.log('tempos por formas por estado');
-    console.log(tempos);
-
     console.log("new ufo_data")
     console.log(new_ufo_data);
-
-
-
-
-
+    console.log(interv_tempo_aux);
 
 
 });
